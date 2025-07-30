5f6eda140f02c45554d5a6c51a0b733ca0e4bbe7
Vladyslav Krykun <mr.laggermpc.890@gmail.com>
2025-07-30 12:45:44 +0200

    setup project

---
+++ AspNetFinalProject/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AspNetFinalProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
